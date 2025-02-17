4cebfcf3f9877d9b80edf4f36ab6c637a40d7c1e
Mico Calingasan <micooo.cali@gmail.com>
2025-02-17 20:02:11 +0800

    Updated complete workout to patch

--- routes/workout.js
+++ routes/workout.js
@@ -9,6 +9,6 @@ router.post("/addWorkout", verify, workoutController.addWorkout);
 router.get("/getMyWorkouts", verify, workoutController.getWorkouts);
 router.put("/updateWorkout/:id", verify, workoutController.updateWorkout);
 router.delete("/deleteWorkout/:id", verify, workoutController.deleteWorkout);
-router.put("/completeWorkoutStatus/:id", verify, workoutController.completeWorkoutStatus);
+router.patch("/completeWorkoutStatus/:id", verify, workoutController.completeWorkoutStatus);
 
 module.exports = router;
