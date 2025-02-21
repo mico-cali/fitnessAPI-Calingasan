68377b6bd88353323ca4b718639d66654bc0eaf3
Mico Calingasan <micooo.cali@gmail.com>
2025-02-22 00:27:28 +0800

    Added cors options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Routes Middleware
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const workoutRoutes = require("./routes/workout");
 const userRoutes = require("./routes/user");
@@ -11,6 +12,21 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const corsOptions = {
+	origin: [
+		"https://fitness-app-client-snowy.vercel.app",
+		"https://fitness-app-client-micocalis-projects.vercel.app",
+		"https://fitness-app-client-git-master-micocalis-projects.vercel.app",
+		"https://fitnessapi-calingasan.onrender.com",
+		"http://localhost:3000",
+	],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 
 //MongoDB database
 mongoose.connect("mongodb+srv://mico_cali:pass123@cluster0.4ipfp.mongodb.net/fitness-API?retryWrites=true&w=majority&appName=Cluster0");
